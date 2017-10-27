08c3ce8993fec1a28b2f8aa975e2c7c39503443a
neo <neo.sunny2k13@gmail.com>
2017-10-27 14:38:27 +0530

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -6,41 +6,33 @@ const dbopr = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-mongoClient.connect(url, (err, db) => {
+mongoClient.connect(url).then((db) => {
 
-    assert.equal(err, null);
     console.log("Connected Correctly to the server");
 
-    dbopr.insertDocument(db, { name: "Vadonut", description: "Test"}, "dishes", (result) => {
-        console.log("Insert documents \n"+ result.ops);
-
-        dbopr.findDocuments(db, "dishes", (docs) => {
-            console.log("Found Documents \n "+ docs);
-
-
-            dbopr.updateDocument(db, {name: "vadonut"}, {description:"Updated Test"}, "dishes", (result) => {
-
-                console.log("Updated Document "+ result.result);
-
-
-             dbopr.findDocuments(db, "dishes", (docs) =>{
-                 console.log("Found updated Documents \n "+ docs);
-
-                 db.dropCollection("dishes", (result) => {
+    dbopr.insertDocument(db, { name: "Vadonut", description: "Test"}, "dishes")
+    .then((result) => {
+        console.log("Insert documents \n", result.ops);
+        return   dbopr.findDocuments(db, "dishes");
+      })
+    .then((docs) => {
+            console.log("Found Documents \n ", docs);
+            return  dbopr.updateDocument(db, {name: "Vadonut"}, {description:"Updated Test"}, "dishes");
+      })
+      .then((result) => {
+                console.log("Updated Document: \n ", result.result);
+                return dbopr.findDocuments(db, "dishes");
+              })
+      .then((docs) => {
+                 console.log("Found updated Documents \n ", docs);
+                 return   db.dropCollection("dishes");
+               })
+      .then((result) => {
                      console.log("Dropped Collection: ", result);
-
-                     db.close();
-                 });
-             });
-
-            });
+                     return db.close();
+                 })
+                 .catch((err) => console.log(err));
 
 
-        });
-
-
-
-    });
-
-    });
-
+})
+.catch((err) => console.log(err));
